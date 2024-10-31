864d2e52dab488056e9533d3b38969d0ffac1499
TranLinh2911 <linhtnph31789@fpt.edu.vn>
2024-10-31 21:39:37 +0700

    Update DiscoutProduct

--- src/main/java/com/example/datn/controllers/admin/ImageController.java
+++ src/main/java/com/example/datn/controllers/admin/ImageController.java
@@ -1,16 +1,17 @@
 package com.example.datn.controllers.admin;
 
 import com.example.datn.entities.Image;
+import com.example.datn.exceptions.NotFoundException;
 import com.example.datn.services.ImageService;
-import javassist.NotFoundException;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-@Controller
+
+@RestController
 @RequestMapping("/admin")
 public class ImageController {
     private final ImageService imageService;
