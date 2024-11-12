acdeb0a4d01b0fe987fd3b57b690beccd7e3effc
thaitv04 <thiavantran204@gmail.com>
2024-11-12 22:43:39 +0700

    update

--- src/main/java/com/example/datn/DatnApplication.java
+++ src/main/java/com/example/datn/DatnApplication.java
@@ -8,7 +8,6 @@ public class DatnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DatnApplication.class, args);
-        System.out.println("runing...");
     }
 
 }
