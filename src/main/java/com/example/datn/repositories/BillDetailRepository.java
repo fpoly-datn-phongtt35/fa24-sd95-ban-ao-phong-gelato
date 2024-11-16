e77fe06b0125a5a48fed6d1c543a3ae337eb5899
DESKTOP-OD1HC6K\Admin <quang>
2024-11-16 22:30:37 +0700

    update validate

--- src/main/java/com/example/datn/repositories/BillDetailRepository.java
+++ src/main/java/com/example/datn/repositories/BillDetailRepository.java
@@ -2,4 +2,5 @@ package com.example.datn.repositories;
 import com.example.datn.entities.BillDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface BillDetailRepository extends JpaRepository<BillDetail, Long>{
+
 }
