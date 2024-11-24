6215de641b6415f991d196961485b8695b6d5d8e
DESKTOP-OD1HC6K\Admin <quang>
2024-11-24 23:00:06 +0700

    update qltk

--- src/main/java/com/example/datn/repositories/AccountRepository.java
+++ src/main/java/com/example/datn/repositories/AccountRepository.java
@@ -4,12 +4,17 @@ package com.example.datn.repositories;
 import com.example.datn.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Account findByEmail(String email);
+    @Query(value = "SELECT * FROM Account WHERE role_id = :roleId", nativeQuery = true)
+    List<Account> findAccountsByRoleIdNative(@Param("roleId") Long roleId);
+    @Query(value = "SELECT * FROM Account WHERE role_id = :roleId", nativeQuery = true)
+    List<Account> findAccountById(@Param("roleId") Long roleId);
 
 
     @Query(value = "SELECT CONCAT('T', MONTH(a.create_date)) AS month, COUNT(a.id) AS count FROM Account a" +
