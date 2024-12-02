07c53c47703f9abf2dd4b4ad1b226224b9e753f9
thaitv04 <thiavantran204@gmail.com>
2024-12-02 15:24:42 +0700

    fix sell online done

--- src/main/java/com/example/datn/services/BillService.java
+++ src/main/java/com/example/datn/services/BillService.java
@@ -66,4 +66,6 @@ public interface BillService {
     Page<BillDto> searchBillJson(SearchBillDto searchBillDto, Pageable pageable);
 
     Page<BillDto> getAllValidBillToReturn( Pageable pageable);
+
+    void deductProductQuantitiesOnStatusChange(Long billId);
 }
