576bfe0f4664cbdcf057c3df6637ed1d82bb38e0
vanthai04 <thaitvph40872@fpt.edu.vn>
2024-12-04 15:49:35 +0700

    update order status done

--- src/main/java/com/example/datn/entities/enumClass/BillStatus.java
+++ src/main/java/com/example/datn/entities/enumClass/BillStatus.java
@@ -2,12 +2,12 @@ package com.example.datn.entities.enumClass;
 
 public enum BillStatus {
 
+    CHO_HANG_VE,
     CHO_XAC_NHAN,
     CHO_LAY_HANG,
     CHO_GIAO_HANG,
-//    DANG_GIAO_HANG,
-
     HOAN_THANH,
+    THAT_BAI,
     HUY,
     TRA_HANG,
     TAI_QUAY
